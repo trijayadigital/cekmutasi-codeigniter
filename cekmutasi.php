e8f1d7b33e1cf4f2781d91266adf0507afee02cc
Suluh Sulistiawan <20874779+sooluh@users.noreply.github.com>
2021-08-10 09:40:33 +0700

    invalid signature fix

--- cekmutasi.php
+++ cekmutasi.php
@@ -1,22 +1,17 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-$config['cekmutasi'] = [
+/**
+ *
+ *	API Key: https://cekmutasi.co.id/app/integration
+ *
+ **/
+$config['api_key'] = 'your_api_key_here';
 
-	/**
-	*
-	*	API Key: https://cekmutasi.co.id/app/integration
-	*
-	**/
-	'api_key'	=> 'your_api_key_here',
-
-
-	/**
-	*
-	*	API Signature: https://cekmutasi.co.id/app/integration
-	*
-	**/
-	'api_signature'	=> 'your_api_signature_here'
-
-];
+/**
+ *
+ *	API Signature: https://cekmutasi.co.id/app/integration
+ *
+ **/
+$config['api_signature'] = 'your_api_signature_here';
